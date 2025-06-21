2b2a3c6dcc60948f44cde2306ff5a413ed46a3df
awovkj <147908510+awovkj@users.noreply.github.com>
2025-06-21 22:49:02 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -20,12 +20,12 @@ module.exports = {
     process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
     'https://cdn.jsdelivr.net/npm/live2d-widget-model-wanko@1.0.5/assets/wanko.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME:
-    process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || true, // 点击宠物挂件切换博客主题
+    process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || false, // 点击宠物挂件切换博客主题
 
   SPOILER_TEXT_TAG: process.env.NEXT_PUBLIC_SPOILER_TEXT_TAG || '', // Spoiler文本隐藏功能，如Notion中 [sp]希望被spoiler的文字[sp]，填入[sp] 即可
 
   // 音乐播放插件
-  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
   MUSIC_PLAYER_VISIBLE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
   MUSIC_PLAYER_AUTO_PLAY:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
@@ -33,23 +33,37 @@ module.exports = {
   MUSIC_PLAYER_CDN_URL:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_CDN_URL ||
     'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/aplayer/1.10.1/APlayer.min.js',
-  MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
+  MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'random', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      name: 'Flower Dance',
+      artist: 'DJOKAWARI',
+      url: 'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/Flower%20Dance%20-%20DJ%20OKAWARI.mp3',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/Snipaste_2024-11-24_20-16-38.png'
     },
     {
-      name: '王都グランセル',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+      name: '記憶',
+      artist: '市川淳',
+      url: 'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/-%E8%A8%98%E6%86%B6-%20(%E8%AE%B0%E5%BF%86)%20-%20%E5%B8%82%E5%B7%9D%E6%B7%B3.mp3',
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    }
+        'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE(43).png'
+    },
+    {
+      name: '三葉のテ一マ',
+      artist: 'RADWIMPS',
+      url: 'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/三葉のテーマ - RADWIMPS.mp3',
+      cover:
+        'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/屏幕截图 2024-11-30 000838.png'
+    },
+    {
+      name: 'スパークル',
+      artist: 'RADWIMPS',
+      url: 'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/スパークル (Movie ver.) - RADWIMPS.mp3',
+      cover:
+        'https://pub-1789f6ed028e4c81b31e6779c00a22de.r2.dev/sanye/屏幕截图 2024-11-30 000838.png'
+    },
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
