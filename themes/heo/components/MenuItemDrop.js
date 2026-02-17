3804f1f87b858b9629bdededac8551d0c1254508
awovkj <3137378443@qq.com>
2026-02-17 21:43:10 +0800

    ertg

--- themes/heo/components/MenuItemDrop.js
+++ themes/heo/components/MenuItemDrop.js
@@ -12,13 +12,15 @@ export const MenuItemDrop = ({ link }) => {
   return (
     <div
       onMouseOver={() => changeShow(true)}
-      onMouseOut={() => changeShow(false)}>
+      onMouseOut={() => changeShow(false)}
+    >
       {/* 不含子菜单 */}
       {!hasSubMenu && (
         <SmartLink
           target={link?.target}
           href={link?.href}
-          className=' hover:bg-black hover:bg-opacity-10 rounded-2xl flex justify-center items-center px-3 py-1 no-underline tracking-widest'>
+          className=' hover:bg-black hover:bg-opacity-10 rounded-2xl flex justify-center items-center px-3 py-1 no-underline tracking-widest'
+        >
           {link?.icon && <i className={link?.icon} />} {link?.name}
         </SmartLink>
       )}
@@ -38,12 +40,14 @@ export const MenuItemDrop = ({ link }) => {
       {hasSubMenu && (
         <ul
           style={{ backdropFilter: 'blur(3px)' }}
-          className={`${show ? 'visible opacity-100 top-14 pointer-events-auto' : 'invisible opacity-0 top-20 pointer-events-none'} drop-shadow-md overflow-hidden rounded-xl bg-white dark:bg-[#1e1e1e] transition-all duration-300 z-20 absolute`}>
+          className={`${show ? 'visible opacity-100 top-14 pointer-events-auto' : 'invisible opacity-0 top-20 pointer-events-none'} drop-shadow-md rounded-xl bg-white dark:bg-[#1e1e1e] transition-all duration-300 z-20 absolute flex items-center px-1 py-1 whitespace-nowrap`}
+        >
           {link.subMenus.map((sLink, index) => {
             return (
               <li
                 key={index}
-                className='cursor-pointer hover:bg-blue-600 dark:hover:bg-yellow-600 hover:text-white text-gray-900 dark:text-gray-100  tracking-widest transition-all duration-200 py-1 pr-6 pl-3'>
+                className='cursor-pointer hover:bg-blue-600 dark:hover:bg-yellow-600 hover:text-white text-gray-900 dark:text-gray-100 tracking-widest transition-all duration-200 px-3 py-1 rounded-lg'
+              >
                 <SmartLink href={sLink.href} target={link?.target}>
                   <span className='text-sm text-nowrap font-extralight'>
                     {link?.icon && <i className={sLink?.icon}> &nbsp; </i>}
